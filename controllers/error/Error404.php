a601a212efbd7749de82e343f4a69a91f5926214
Adrien <officielghostfire@gmail.com>
2017-10-02 00:49:55 +0200

    re-upload v1

---
+++ controllers/error/Error404.php
@@ -0,0 +1,12 @@
+<?php
+namespace Project\Controllers\Error;
+
+class Error404
+{
+    public static function index($twig)
+    {
+        $array = [];
+
+        return $twig->render('404.html.twig', $array);
+    }
+}
