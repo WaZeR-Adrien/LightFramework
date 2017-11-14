e787c7edb21af780819d050e06b163d1d0df7109
WaZeR [Adrien] <officielghostfire@gmail.com>
2017-11-14 19:41:52 +0100

    Update Config-sample.php

--- kernel/Config-sample.php
+++ kernel/Config-sample.php
@@ -10,12 +10,22 @@ class Config
             'user' => 'YOUR_USER',
             'pw'   => 'YOUR_PW'
         ],
+        'reg' => [
+            'int'       => '/^[0-9]+$/i',
+            'strint'    => '/^[a-zA-Z0-9 ]+$/i',
+            'strintacc' => '/^[\p{L}\p{Nd}\s-_]+$/i',
+        ],
         'path' => []
     ];
 
     public static function getDatabase()
     {
         return self::$_config['database'];
+    }
+    
+    public static function getReg()
+    {
+        return self::$_config['reg'];
     }
 
     public static function getPath()
